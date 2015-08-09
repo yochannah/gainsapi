b3033dac577d79d10d9ff7f62395cc4226f946f1
Yochannah Yehudi <yochannah@gmail.com>
2015-08-09 17:18:02 +0100

    updated to cope with update type reports but currently buggy.

--- src/main/java/com/digitalcranberry/gainslapi/StoreReport.java
+++ src/main/java/com/digitalcranberry/gainslapi/StoreReport.java
@@ -1,8 +1,10 @@
 package com.digitalcranberry.gainslapi;
 
+import com.digitalcranberry.gainslapi.model.Report;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
@@ -26,52 +28,50 @@ public class StoreReport extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
-    UserService userService = UserServiceFactory.getUserService();
-    User user = userService.getCurrentUser();
 
     final Logger log = Logger.getLogger(StoreReport.class.getName());
-    
-    String orgName = 	propertyOrDefault(req, "orgName", "OU");
-    String status = 	propertyOrDefault(req, "status", "new");
-    String reportid = 	req.getParameter("reportid");    
-    String latitude = 	req.getParameter("latitude");
-    //String dc = 		req.getParameter("dc");
-    String longitude = 	req.getParameter("longitude");
-    String content = 	req.getParameter("content");
-    String dateCaptured = req.getParameter("dateCaptured");
 
-    Date date = new Date();  
-    System.out.println("date captured: " + dateCaptured);
-    //log.info("dc: " + dc);
-    Date dateCreated = dateFromReport(dateCaptured);
-    Date dateReceived = new Date(); //we've received it now :) 
-    
-    Key reportStoreKey = KeyFactory.createKey("gainsl", reportid);
-    
-    Entity report = new Entity("Report", reportStoreKey);
-    if (user != null) {
-      report.setProperty("author_id", user.getUserId());
-      report.setProperty("author_email", user.getEmail());
-    }
-    
-    report.setProperty("latitude", latitude);
-    report.setProperty("longitude", longitude);
-    report.setProperty("content", content);
-    report.setProperty("reportid", reportid);
-    report.setProperty("dateCreated", dateCreated);
-    report.setProperty("date", date);
-    report.setProperty("status", status);
-    
+    Entity newReport = setReportDetails(req);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(report);
-
-    if(user != null) {
-    	resp.sendRedirect("/gainslapi.jsp?orgName=" + orgName);
+        
+    Entity existingReport = checkForExisting((Key) newReport.getProperty("reportid"), datastore);
+    if(existingReport == null) {
+        //this is a new report, just add it to the store
+    	datastore.put(newReport);
     } else {
-    	resp.sendRedirect("/reportList?orgName=" + orgName);
+        //this is an update to a report. We need to check if it's appropriate to over-write.
+
+    	Date existingLastUpdated = (Date) existingReport.getProperty("lastUpdated");
+      Date newLastUpdated = (Date) newReport.getProperty("lastUpdated");
+    	if (existingLastUpdated.after(newLastUpdated)) {
+      	//this means that a newer report arrived before
+    		//the report we've just received, so we want to
+    		//store the report we've just received as a previous state, and -not-
+    		//overwrite anything.
+        log.info("We've received a report, but there's a newer one than this" +
+         "in the Datastore already. Archiving the report we just received.");
+
+    	} else {
+    		//this means that the report we're just received is newer than the
+    		//current report state. We'll save the existing state in the previous state list
+    		//and then overwrite the existing state with the values we just received.
+        log.info("We've received a report that is an update. Updating now.//TODO");
+    	}
+
+    }
+
+    datastore.put(newReport);
+
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+    
+    if(user != null) {
+    	resp.sendRedirect("/gainslapi.jsp?orgName=" + newReport.getProperty("orgName"));
+    } else {
+    	resp.sendRedirect("/reportList?orgName=" + newReport.getProperty("orgName"));
     }
   }
-  
+
   private String propertyOrDefault(HttpServletRequest req, String propName, String defaultValue) {
 	  String value = req.getParameter(propName);
 	  if (value == null) {
@@ -79,8 +79,63 @@ public class StoreReport extends HttpServlet {
 	  }
 	  return value;
   }
-  
-  private Date dateFromReport(String dateString){
-	 return new Date(Long.parseLong(dateString)); 
+
+  private Date dateFromString(String dateString){
+	 return new Date(Long.parseLong(dateString));
   }
+
+  /*
+   * Checks for report with this ID and returns it if it exists
+   * Or null, otherwise.
+   */
+  private Entity checkForExisting(Key reportId, DatastoreService datastore){
+	  Entity report = null;
+	  try {
+			report = datastore.get(reportId);
+	  } catch (EntityNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	  }
+	  return report;
+  }
+
+  private Entity setReportDetails(HttpServletRequest req){
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+
+    String orgName = 	propertyOrDefault(req, "orgName", "OU");
+    String status = 	propertyOrDefault(req, "status", "new");
+    String reportid = 	req.getParameter("reportid");
+    String latitude = 	req.getParameter("latitude");
+    String longitude = 	req.getParameter("longitude");
+    String content = 	req.getParameter("content");
+
+    String dateFirstCapturedStr = req.getParameter("dateFirstCaptured");
+    String lastUpdateStr = req.getParameter("lastUpdated");
+
+    Date date = new Date();
+    Date dateFirstCaptured = dateFromString(dateFirstCapturedStr);
+    Date dateReceived = new Date(); //we've received it now :)
+    Date lastUpdated = dateFromString(lastUpdateStr); //we've received it now :)
+
+    //set report ID to match the ID sent by the user.
+    Key reportStoreKey = KeyFactory.createKey("gainsl", reportid);
+
+    Entity report = new Entity("Report", reportStoreKey);
+    if (user != null) {
+      report.setProperty("author_id", user.getUserId());
+      report.setProperty("author_email", user.getEmail());
+    }
+
+    report.setProperty("latitude", latitude);
+    report.setProperty("longitude", longitude);
+    report.setProperty("content", content);
+    report.setProperty("reportid", reportid);
+    report.setProperty("dateCreated", dateFirstCaptured);
+    report.setProperty("lastUpdated", lastUpdated);
+    report.setProperty("date", date);
+    report.setProperty("status", status);
+    return report;
+  }
+
 }
